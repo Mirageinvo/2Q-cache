c4c62f5d5bb004a735d01f09b5ca990620700bcb
Mirage <andrej_malinin@mail.ru>
2021-11-16 16:59:24 +0300

    fixed tests

--- tests/tests_for_caches.cpp
+++ tests/tests_for_caches.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <stdio.h>
 #include "gtest/gtest.h"
-#include "../Cache/includes/ideal_cache.hpp"
-#include "../Cache/includes/ideal_cache_impl.hpp"
-#include "../Cache/includes/cache_2q.hpp"
-#include "../Cache/includes/cache_2q_impl.hpp"
+#include "ideal_cache.hpp"
+#include "ideal_cache_impl.hpp"
+#include "cache_2q.hpp"
+#include "cache_2q_impl.hpp"
 
 TEST(IdealCache, TestOne) {
     size_t cache_capacity, request_num;
@@ -126,6 +126,185 @@ TEST(IdealCache, TestTen) {
     EXPECT_EQ(cache.number_of_hits(), 19680);
 }
 
+TEST(Cache2Q, TestOne) {
+    size_t cache_capacity, request_num;
+    int num;
+    FILE* f;
+    f = fopen("../../tests/files_for_tests/003.dat","r");
+    fscanf(f, "%lu%lu", &cache_capacity, &request_num);
+    cache_2q<int> cache(cache_capacity);
+    for (size_t i = 0; i < request_num; ++i) {
+        fscanf(f, "%d", &num);
+        cache.put(num);
+    }
+    fclose(f);
+    EXPECT_EQ(cache.number_of_hits(), 3);
+}
+
+TEST(Cache2Q, TestTwo) {
+    size_t cache_capacity, request_num;
+    int num;
+    FILE* f;
+    f = fopen("../../tests/files_for_tests/004.dat","r");
+    fscanf(f, "%lu%lu", &cache_capacity, &request_num);
+    cache_2q<int> cache(cache_capacity);
+    for (size_t i = 0; i < request_num; ++i) {
+        fscanf(f, "%d", &num);
+        cache.put(num);
+    }
+    fclose(f);
+    EXPECT_EQ(cache.number_of_hits(), 10);
+}
+
+TEST(Cache2Q, TestThree) {
+    size_t cache_capacity, request_num;
+    int num;
+    FILE* f;
+    f = fopen("../../tests/files_for_tests/005.dat","r");
+    fscanf(f, "%lu%lu", &cache_capacity, &request_num);
+    cache_2q<int> cache(cache_capacity);
+    for (size_t i = 0; i < request_num; ++i) {
+        fscanf(f, "%d", &num);
+        cache.put(num);
+    }
+    fclose(f);
+    EXPECT_EQ(cache.number_of_hits(), 32);
+}
+
+TEST(Cache2Q, TestFour) {
+    size_t cache_capacity, request_num;
+    int num;
+    FILE* f;
+    f = fopen("../../tests/files_for_tests/006.dat","r");
+    fscanf(f, "%lu%lu", &cache_capacity, &request_num);
+    cache_2q<int> cache(cache_capacity);
+    for (size_t i = 0; i < request_num; ++i) {
+        fscanf(f, "%d", &num);
+        cache.put(num);
+    }
+    fclose(f);
+    EXPECT_EQ(cache.number_of_hits(), 187);
+}
+
+TEST(Cache2Q, TestFive) {
+    size_t cache_capacity, request_num;
+    int num;
+    FILE* f;
+    f = fopen("../../tests/files_for_tests/007.dat","r");
+    fscanf(f, "%lu%lu", &cache_capacity, &request_num);
+    cache_2q<int> cache(cache_capacity);
+    for (size_t i = 0; i < request_num; ++i) {
+        fscanf(f, "%d", &num);
+        cache.put(num);
+    }
+    fclose(f);
+    EXPECT_EQ(cache.number_of_hits(), 469);
+}
+
+TEST(Cache2Q, TestSix) {
+    size_t cache_capacity, request_num;
+    int num;
+    FILE* f;
+    f = fopen("../../tests/files_for_tests/008.dat","r");
+    fscanf(f, "%lu%lu", &cache_capacity, &request_num);
+    cache_2q<int> cache(cache_capacity);
+    for (size_t i = 0; i < request_num; ++i) {
+        fscanf(f, "%d", &num);
+        cache.put(num);
+    }
+    fclose(f);
+    EXPECT_EQ(cache.number_of_hits(), 3301);
+}
+
+TEST(Cache2Q, TestSeven) {
+    size_t cache_capacity, request_num;
+    int num;
+    FILE* f;
+    f = fopen("../../tests/files_for_tests/009.dat","r");
+    fscanf(f, "%lu%lu", &cache_capacity, &request_num);
+    cache_2q<int> cache(cache_capacity);
+    for (size_t i = 0; i < request_num; ++i) {
+        fscanf(f, "%d", &num);
+        cache.put(num);
+    }
+    fclose(f);
+    EXPECT_EQ(cache.number_of_hits(), 953);
+}
+
+TEST(Cache2Q, TestEight) {
+    size_t cache_capacity, request_num;
+    int num;
+    FILE* f;
+    f = fopen("../../tests/files_for_tests/010.dat","r");
+    fscanf(f, "%lu%lu", &cache_capacity, &request_num);
+    cache_2q<int> cache(cache_capacity);
+    for (size_t i = 0; i < request_num; ++i) {
+        fscanf(f, "%d", &num);
+        cache.put(num);
+    }
+    fclose(f);
+    EXPECT_EQ(cache.number_of_hits(), 4984);
+}
+
+TEST(Cache2Q, TestNine) {
+    size_t cache_capacity, request_num;
+    int num;
+    FILE* f;
+    f = fopen("../../tests/files_for_tests/011.dat","r");
+    fscanf(f, "%lu%lu", &cache_capacity, &request_num);
+    cache_2q<int> cache(cache_capacity);
+    for (size_t i = 0; i < request_num; ++i) {
+        fscanf(f, "%d", &num);
+        cache.put(num);
+    }
+    fclose(f);
+    EXPECT_EQ(cache.number_of_hits(), 89999);
+}
+
+TEST(Cache2Q, TestTen) {
+    size_t cache_capacity, request_num;
+    int num;
+    FILE* f;
+    f = fopen("../../tests/files_for_tests/012.dat","r");
+    fscanf(f, "%lu%lu", &cache_capacity, &request_num);
+    cache_2q<int> cache(cache_capacity);
+    for (size_t i = 0; i < request_num; ++i) {
+        fscanf(f, "%d", &num);
+        cache.put(num);
+    }
+    fclose(f);
+    EXPECT_EQ(cache.number_of_hits(), 110508);
+}
+
+TEST(Cache2Q, TestEleven) {
+    size_t cache_capacity, request_num;
+    int num;
+    FILE* f;
+    f = fopen("../../tests/files_for_tests/013.dat","r");
+    fscanf(f, "%lu%lu", &cache_capacity, &request_num);
+    cache_2q<int> cache(cache_capacity);
+    for (size_t i = 0; i < request_num; ++i) {
+        fscanf(f, "%d", &num);
+        cache.put(num);
+    }
+    fclose(f);
+    EXPECT_EQ(cache.number_of_hits(), 298999);
+}
+
+TEST(Cache2Q, TestTwelve) {
+    size_t cache_capacity, request_num;
+    int num;
+    FILE* f;
+    f = fopen("../../tests/files_for_tests/014.dat","r");
+    fscanf(f, "%lu%lu", &cache_capacity, &request_num);
+    cache_2q<int> cache(cache_capacity);
+    for (size_t i = 0; i < request_num; ++i) {
+        fscanf(f, "%d", &num);
+        cache.put(num);
+    }
+    fclose(f);
+    EXPECT_EQ(cache.number_of_hits(), 389999);
+}
 
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
