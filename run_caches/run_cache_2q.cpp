f9fe11ab30174355bf3029744b9ed73fea7494cc
Mirage <andrej_malinin@mail.ru>
2021-11-21 00:47:19 +0300

    improved capacity cases

--- run_caches/run_cache_2q.cpp
+++ run_caches/run_cache_2q.cpp
@@ -8,14 +8,21 @@ int main() {
   size_t cache_capacity, request_num;
   int num, num_of_hits = 0;
   std::cin >> cache_capacity >> request_num;
-  assert(cache_capacity >= 3);
+  if (cache_capacity < 3) {
+    std::cout << std::endl
+              << "Inappropriate cache capacity" << std::endl
+              << "Cache capacity is less than 3" << std::endl
+              << std::endl;
+  } else {
+    assert(cache_capacity >= 3);
 
-  cache_2q<int> cache(cache_capacity);
-  for (size_t i = 0; i < request_num; ++i) {
-    std::cin >> num;
-    num_of_hits += cache.check_in(num);
+    cache_2q<int> cache(cache_capacity);
+    for (size_t i = 0; i < request_num; ++i) {
+      std::cin >> num;
+      num_of_hits += cache.check_in(num);
+    }
+
+    std::cout << num_of_hits << std::endl;
   }
-
-  std::cout << num_of_hits << std::endl;
   return 0;
 }
